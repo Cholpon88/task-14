6af6c65253a3165dfccc4fcdb93672dcce8b435f
Cholpon Medetkulova <87806846+Cholpon88@users.noreply.github.com>
2022-03-19 18:38:36 +0600

    Update script.js
    added commit

--- task-14/script.js
+++ task-14/script.js
@@ -1,13 +1,6 @@
-let menuBtn = document.querySelector(".menu-btn");
+const toggleButton = document.getElementsByClassName('toggle__button')[0]
+const navbarLinks = document.getElementsByClassName('navbar__links')[0]
 
-let menu = document.querySelector(".header-nav");
-
-menuBtn.addEventListener("click", function(){
-    menu.classList.toggle("active");
-
-    if (menu.classList.contains("active")){
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-});
+toggleButton.addEventListener('click', function () {
+    navbarLinks.classList.toggle('active')
+})
